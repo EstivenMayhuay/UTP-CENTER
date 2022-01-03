54833b6cc34869e8a4c14e8edcff33d4b7ed4493
EstivenMayhuay <developerwebhairton@gmail.com>
2022-01-03 12:48:22 -0500

    add the functionlity

---
+++ public/script/app.js
@@ -0,0 +1,36 @@
+(function () {
+  let lists = Array.from(
+    document.getElementsByClassName("list__button--click")
+  );
+
+  lists.forEach((list) => {
+    list.addEventListener("click", function () {
+      let subMenu = this.nextElementSibling;
+      let children = Array.from(this.children);
+      let iconArrow = children[1];
+      let height = 0;
+
+      iconArrow.classList.toggle("arrow");
+
+      if (subMenu.clientHeight == 0) height = subMenu.scrollHeight;
+
+      subMenu.style.height = `${height}px`;
+    });
+  });
+})();
+
+(function () {
+  let iconMenu = document.querySelector(".iconMenu");
+
+  iconMenu.addEventListener("click", function () {
+    let menu = this.nextElementSibling;
+    let parentMenu = this.parentElement;
+    let height = 0;
+
+    parentMenu.classList.toggle("hidden");
+
+    if (menu.clientHeight == 48) height = menu.scrollHeight;
+
+    menu.style.height = `${height}px`;
+  });
+})();
