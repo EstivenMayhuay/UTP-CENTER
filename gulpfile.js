bd8be74681171befd77167fdde7828f813856c85
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-02-15 09:54:16 -0500

    fix the responsive design

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,13 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
 
-// create task for pug
 gulp.task("pug", function () {
   return gulp
     .src("src/pug/pages/*.pug")
-    .pipe(
-      pug({
-        pretty: true,
-      })
-    )
+    .pipe(pug({ pretty: true }))
     .pipe(gulp.dest("public/"));
 });
 
 gulp.task("default", function () {
-  gulp.watch("src/pug/pages/*.pug", gulp.series("pug"));
+  gulp.watch("src/pug/pages/*pug", gulp.series("pug"));
 });
--- public/script/modules/menu_hamburger.js
+++ public/script/modules/menu_hamburger.js
@@ -4,6 +4,8 @@ export default function hamburger(menuNav, btnMenu) {
   d.addEventListener("click", (e) => {
     if (e.target.matches(btnMenu) || e.target.matches(`${btnMenu} *`)) {
       d.querySelector(menuNav).classList.toggle("active-nav");
+      d.querySelector(btnMenu).classList.toggle("activeIconMenu");
+      //console.log(d.querySelector(btnMenu));
     }
   });
 }
